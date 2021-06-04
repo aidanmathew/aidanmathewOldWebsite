ec3f449fd1a60760b29485d26311ed81607f2531
Aidan Mathew <aidanmathew@Aidans-MacBook-Air.local>
2021-06-04 14:40:37 -0400

    added navbar effects

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Mobile Menu -> Hamburger
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".nav-menu");
 
@@ -5,3 +6,15 @@ menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+// Hides the navbar when scrolling
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-5rem";
+  }
+  prevScrollpos = currentScrollPos;
+};
