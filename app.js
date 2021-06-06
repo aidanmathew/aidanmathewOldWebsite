96cd6731be44d342ca1d9e7542384ee868a1c15c
Aidan Mathew <aidanmathew@Aidans-MacBook-Air.local>
2021-06-06 12:57:57 -0400

    added TYPEIT

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 // Mobile Menu -> Hamburger
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".nav-menu");
@@ -18,3 +20,32 @@ window.onscroll = function () {
   }
   prevScrollpos = currentScrollPos;
 };
+
+// AOS
+AOS.init();
+
+// Typo
+new TypeIt("#type1", {
+  speed: 90,
+  loop: true,
+  waitUntilVisible: true,
+})
+  .type("a Computer Engineering Student.", { delay: 400 })
+  .pause(1000)
+  .delete()
+  .type("a Web Developer.", { delay: 400 })
+  .pause(1000)
+  .delete()
+  .type("an Athlete.", { delay: 400 })
+  .pause(1000)
+  .delete()
+  .type("a Software Developer.", { delay: 400 })
+  .pause(1000)
+  .delete()
+  .type("a Problem Solver.", { delay: 400 })
+  .pause(1000)
+  .delete()
+  .type("an Analytical Thinker", { delay: 400 })
+  .pause(1000)
+  .delete()
+  .go();
